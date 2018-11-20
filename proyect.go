db009d110bf9732d468844604a01012e1f99a70f
Eddy <eddyeu59@gmail.com>
2018-11-20 00:25:37 -0400

    finish
    run proyect

--- proyect.go
+++ proyect.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -16,20 +19,38 @@ func main() {
 	textFile := string(dataBin)
 	wc := getWordsCountMapOfFile(textFile)
 	w := getStringSortWordsOfWordsCountMap(wc)
-	for _, word := range w {
+	textFile = ""
+	for i, word := range w {
 		fmt.Println(word, wc[word])
+		result := strconv.Itoa(wc[word])
+		ini := strconv.Itoa(i + 1)
+		textFile += "[" + ini + "] " + word + " : " + result + "\n"
 	}
+	writeToFile(textFile)
 }
 
 // getWordsCountMapOfFile : here
 func getWordsCountMapOfFile(textFile string) map[string]int {
 	lowerText := strings.ToLower(textFile)
 
-	reg, err := regexp.Compile("[^a-zA-Z]+")
+	lowerTextClean1 := strings.Replace(lowerText, "`", "", -1)
+	lowerTextClean2 := strings.Replace(lowerTextClean1, "'", "", -1)
+	lowerTextClean3 := strings.Replace(lowerTextClean2, ",", "", -1)
+	lowerTextClean4 := strings.Replace(lowerTextClean3, ".", "", -1)
+	lowerTextClean5 := strings.Replace(lowerTextClean4, " - ", "", -1)
+	lowerTextClean6 := strings.Replace(lowerTextClean5, " -", "", -1)
+	lowerTextCleanFull := strings.Replace(lowerTextClean6, "- ", "", -1)
+
+	//reg, err := regexp.Compile("[^a-zA-Z ]+$") //516 words  456
+	reg, err := regexp.Compile("[^a-zA-z0-9]+$") //516 words
+	//reg, err := regexp.Compile("[^a-zA-Z]+")//440 words
+	//reg, err := regexp.Compile("[^a-zA-Z0-9]+") //440 words, output-ee.txt'
+
 	if err != nil {
 		fmt.Print(err)
+		panic(err)
 	}
-	onlyAlphaText := reg.ReplaceAllString(lowerText, " ")
+	onlyAlphaText := reg.ReplaceAllString(lowerTextCleanFull, " ")
 	words := strings.Fields(onlyAlphaText)
 
 	wordsCountMap := make(map[string]int)
@@ -47,4 +68,14 @@ func getStringSortWordsOfWordsCountMap(wordsCountMap map[string]int) []string {
 	}
 	sort.Strings(words)
 	return words
+}
+
+func writeToFile(textToFile string) {
+	file, err := os.Create("result.txt")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, textToFile)
 }
